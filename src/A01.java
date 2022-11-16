aad93745dfbc833b3355914abe0af28f464348d8
fatihruya <57195656+fatihruya@users.noreply.github.com>
2022-11-16 14:33:26 +0000

    from intellij

--- src/A01.java
+++ src/A01.java
@@ -3,6 +3,7 @@ public class A01 {
     public static void main(String[] args) {
         System.out.println("Github lesson starts");
                 System.out.println("this is created in gitHub");
+        System.out.println("this is coming from intellij");
 
         
         
