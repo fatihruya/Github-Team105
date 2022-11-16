42a713b7c0e85995d798fa0d7d1389612c312f56
fatihruya <57195656+fatihruya@users.noreply.github.com>
2022-11-16 14:29:00 +0000

    Update A01.java

--- src/A01.java
+++ src/A01.java
@@ -2,5 +2,9 @@ public class A01 {
 
     public static void main(String[] args) {
         System.out.println("Github lesson starts");
+                System.out.println("this is created in gitHub");
+
+        
+        
     }
 }
